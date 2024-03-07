63bf201b17627bebf1c4a4218a8f97da34a38958
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-03-07 09:36:50 -0500

    add buttons to .js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // script.js for choose-wisely
+
+let pageNumber=0;
+
+const button1 = document.getElementById("button1");
+button1.addEventListener("click", ()=> {
+    console.log("clicked button1");
+});
+
+const button2 = document.getElementById("button2");
+button2.addEventListener("click", ()=> {
+    console.log("clicked button2");
+});
