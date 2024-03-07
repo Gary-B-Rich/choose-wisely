7d271a2974a9347df32874b0bd6f5bbe99de47e2
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-03-07 10:03:08 -0500

    tweaks

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 
 let pageNumber=0;
 
+const bigTitleArray=[`Choose Your Own Fate`];
+const smallTitleArray=[`By Gary B. Rich`];
+const textArray=[`Play Now?`];
+const button1Array=[`Play`];
+const button2Array=[`Play`];
+
 const button1 = document.getElementById("button1");
 button1.addEventListener("click", ()=> {
     console.log("clicked button1");
@@ -11,3 +17,10 @@ const button2 = document.getElementById("button2");
 button2.addEventListener("click", ()=> {
     console.log("clicked button2");
 });
+
+let x=0;
+console.log(bigTitleArray[x]);
+console.log(smallTitleArray[x]);
+console.log(textArray[x]);
+console.log(button1Array[x]);
+console.log(button2Array[x]);
