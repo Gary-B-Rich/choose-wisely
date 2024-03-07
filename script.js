41b67e1748909452be5fec3a2dd9dadf77831a3c
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-03-07 14:32:14 -0500

    page 4 working

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
 
 // arrays for each div text
-const bigTitleArray=[`Choose Your Own Fate`,`One`,`Two`,`Three`];
-const smallTitleArray=[`By Gary B. Rich`,`Your Story Begins`,`Staying put`,`Foot it`];
+const bigTitleArray=[`Choose Your Own Fate`,`One`,`Two`,`Three`,`Four`];
+const smallTitleArray=[`By Gary B. Rich`,`Your Story Begins`,`Staying put`,`Foot it`,`Pathological`];
 const textArray=[`Play now?`,`It is an early summer evening, just past nightfall.
 Stars fill the sky, as a small crescent moon offers little light.\n
 You are driving through a remote area with only the radio to keep you company.
@@ -32,9 +32,14 @@ Another 30 minutes pass.  Do you want to keep waiting?`,`You lock the car and st
 Both sides are lined with dense forest.\n
 After what feels like about an hour, you come across a dirt path leading into the woods.\n
 You can't see where the path leads, but you notice a distant light flickering in that direction.\n
-Do you want to stay on the main road, or do you want to see if the path leads to someone who can help?`];
-const button1Array=[`Play`,`Wait`,`Wait`,`Road`];
-const button2Array=[`Play`,`Walk`,`Walk`,`Path`];
+Do you want to stay on the main road, or do you want to see if the path leads to someone who can help?`,
+`Leaving the main road, you start walking down the dirt path into the deep woods.
+Trees have overgrown the sides, and branches scratch your face and arms in the dark.\n
+As you continue on the winding path, the shape of a small building begins to form.
+You realize the flickering light is coming from the window of a small cabin.\n
+Do you want to approach the cabin, or do you decide to quietly go back to the main road?`];
+const button1Array=[`Play`,`Wait`,`Wait`,`Road`, `Cabin`];
+const button2Array=[`Play`,`Walk`,`Walk`,`Path`, `Road`];
 
 // render all text in the dom at pageload
 updatePage();
