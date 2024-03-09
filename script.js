1b89666d748337796fc5205403512f10c6fd5f25
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-03-09 15:25:16 -0500

    lots of pages working

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
 
 // arrays for each div text
-const bigTitleArray=[`Choose Your Own Fate`,`One`,`Two`,`Three`,`Four`];
-const smallTitleArray=[`By Gary B. Rich`,`Your Story Begins`,`Staying put`,`Foot it`,`Pathological`];
+const bigTitleArray=[`Choose Your Own Fate`,`One`,`Two`,`Three`,`Four`,`Five`,`Six`,`Seven`];
+const smallTitleArray=[`By Gary B. Rich`,`Your Story Begins`,`Staying put`,`Foot it`,`Pathological`,
+`Cabin in the Woods`,`Knock knock who's there`,`Nope Out`];
 const textArray=[`Play now?`,`It is an early summer evening, just past nightfall.
 Stars fill the sky, as a small crescent moon offers little light.\n
 You are driving through a remote area with only the radio to keep you company.
@@ -37,14 +38,29 @@ Do you want to stay on the main road, or do you want to see if the path leads to
 Trees have overgrown the sides, and branches scratch your face and arms in the dark.\n
 As you continue on the winding path, the shape of a small building begins to form.
 You realize the flickering light is coming from the window of a small cabin.\n
-Do you want to approach the cabin, or do you decide to quietly go back to the main road?`];
-const button1Array=[`Play`,`Wait`,`Wait`,`Road`, `Cabin`];
-const button2Array=[`Play`,`Walk`,`Walk`,`Path`, `Road`];
+Do you want to approach the cabin, or do you decide to quietly go back to the main road?`,
+`You quietly walk up to the cabin until you're standing just outside the door.
+The only sounds you hear are the crickets chirping.  Other than that, everything seems quiet.\n
+The immediate area around the cabin gives the impression that someone is currently living here.
+Around the side of the cabin, light is still flickering in the only window.\n
+Do you want to try looking in the window, or do you want to knock on the door?`,
+`You loudly knock on the door several times, and wait.\n
+You still don't hear anything, and nobody answers.\n\n
+Do you want to try opening the door, or would you rather just go back to the main road?`,
+`Nobody seems to be home, so you turn around and start heading back to the main road.\n
+About 50 yards from the cabin you step in a bear trap that you didn't see in the dim moonlight.\n
+You try to free yourself, but the trap is firmly lodged in your broken leg.<br>
+\n
+Nobody finds you in time.`];
+const button1Array=[`Play`,`Wait`,`Wait`,`Road`, `Cabin`,`Window`,`Door`,`Play`];
+const button2Array=[`Play`,`Walk`,`Walk`,`Path`, `Road`,`Door`,`Road`,`Again`];
 
 // render all text in the dom at pageload
 updatePage();
 
 // add functions to buttons
+
+// BUTTON 1
 button1.addEventListener("click", ()=> {
     console.log("clicked button1");
     switch(pageNumber) {
@@ -53,6 +69,15 @@ button1.addEventListener("click", ()=> {
             break;
         case 1:
             pageNumber=2;
+            break;
+        case 2:
+            pageNumber=undefined;
+            break;
+        case 3:
+            pageNumber=undefined;
+            break;
+        case 4:
+            pageNumber=5;
             break;
     }
 
@@ -78,6 +103,19 @@ button2.addEventListener("click", ()=> {
         case 1:
             pageNumber=3;
             break;
+        case 2:
+            pageNumber=3;
+            break;
+        case 3:
+            pageNumber=4;
+            break;
+        case 4:
+            pageNumber=undefined;
+        case 5:
+            pageNumber=6;
+            break;
+        case 6:
+            pageNumber=7;
     }
     
     //button clicks one page up from current page ****
