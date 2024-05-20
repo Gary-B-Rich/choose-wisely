2aecba52dc543616832c1456dd83748e8015f2a5
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-05-20 15:02:25 -0400

    add to button1

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ There you find everything you need to get your tire repaired.`,//end page 17
 
 ];
 
-const button1Array=[`Play`,`Wait`,`Wait`,`Road`, `Cabin`,`Look`,`Door`,`Play`,`Road`,`Talk`,`Play`,`Play`,`Play`,`Ride`,`Play`,`Play`,`Bell`,`Play`,`*18*`,];
+const button1Array=[`Play`,`Wait`,`Wait`,`Road`, `Cabin`,`Look`,`Door`,`Play`,`Road`,`Talk`,`Play`,`Play`,`Play`,`Ride`,`Play`,`Play`,`Bell`,`Play`,`*18*`,`*19*`,`*20*`];
 
 const button2Array=[`Play`,`Walk`,`Walk`,`Path`, `Road`,`Door`,`Road`,`Again`,`Wait`,`Run!`,`Again`,`Again`,`Again`,`Pass`,`Again`,`Again`,`Walk`,`Again`,`*18*`];
 
