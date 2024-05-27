3b16f90c23a0374eef1095f08e141744a6951a8d
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-05-27 00:35:35 -0400

    button1 on page 8 redirects to page 7 working

--- script.js
+++ script.js
@@ -169,7 +169,7 @@ button1.addEventListener("click", ()=> {
             pageNumber=1;
             break;
         case 8:
-            pageNumber=undefined; //fix me
+            pageNumber=7;
             break;
         case 9:
             pageNumber=12;
