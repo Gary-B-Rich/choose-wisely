1205be54d1d2bef66b73800fd0dd06f3c6cc00c8
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-05-30 00:54:09 -0400

    page 5 good

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ const button2Array=[`Play`,`Walk`,`Walk`,`Path`, `Road`,`Door`,`Road`,`Again`,`W
 
 // render all text in the dom at pageload
 
-pageNumber=4;
+pageNumber=5;
 updatePage();
 
 // add functions to buttons
