7b46f091d92c4886418dcc48c72940b0c2f63885
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-06-28 00:02:14 -0400

    fix bug

--- script.js
+++ script.js
@@ -230,7 +230,7 @@ button1.addEventListener("click", ()=> {
 // BUTTON 2
 button2.addEventListener("click", ()=> {
     console.log("clicked button2");
-    //bigTitle.hidden="hidden";
+    bigTitle.hidden="hidden";
     filler.hidden="hidden";
     switch(pageNumber) {
         case 0:
